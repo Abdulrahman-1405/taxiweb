80d99fa56a44ac0f29d1ead5fe75f031c55f5f04
Abdulrahman-1405 <ashifa@mutualart.com>
2026-08-01 21:32:10 +0530

    initial git

---
+++ js/config.js
@@ -0,0 +1,6 @@
+/**
+ * Site-wide config.
+ * Change API_BASE_URL to your deployed backend's URL once it's live on Render.
+ * Example: 'https://silver-taxi-backend.onrender.com/api'
+ */
+window.API_BASE_URL = 'http://localhost:5000/api';
