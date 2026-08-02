ddb7ab080e0a521086c0218da562eec01a75c609
Abdulrahman-1405 <ashifa@mutualart.com>
2026-08-02 16:49:58 +0530

    updated site

--- js/config.js
+++ js/config.js
@@ -3,4 +3,4 @@
  * Change API_BASE_URL to your deployed backend's URL once it's live on Render.
  * Example: 'https://silver-taxi-backend.onrender.com/api'
  */
-window.API_BASE_URL = 'https://backendtaxiweb.onrender.com';
+window.API_BASE_URL = 'https://backendtaxiweb.onrender.com/api';
